3b7a519d21c0f960edc003f9c3b78b3575927dda
lngdao <longdao.work@gmail.com>
2026-04-02 17:06:12 +0700

    fix: review panel resizable, remove invalid web-component import

--- src/openacp/app.tsx
+++ src/openacp/app.tsx
@@ -20,13 +20,13 @@ function ChatArea() {
   return (
     <div class="flex flex-1 min-h-0 h-full min-w-0">
       <div class="@container relative flex-1 flex flex-col min-h-0 h-full bg-background-stronger min-w-0">
-        <ChatView onOpenReview={() => setReviewOpen(true)} />
+        <ChatView onOpenReview={() => { console.log("[review] setReviewOpen(true)"); setReviewOpen(true) }} />
         <Show when={chat.activeSession()}>
           <Composer />
         </Show>
       </div>
       <Show when={reviewOpen()}>
-        <div class="shrink-0 h-full" style={{ width: "480px" }}>
+        <div class="shrink-0 h-full">
           <ReviewPanel onClose={() => setReviewOpen(false)} />
         </div>
       </Show>
